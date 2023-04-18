b7767d838905a90a534a988e0a2d56f76fb3ef86
sahudev <devendra.sahu19@gmail.com>
2023-04-18 14:33:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Products from "./Products";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Products />
+    </div>
+  );
+}
---
+++ src/Products.jsx
@@ -0,0 +1,11 @@
+import ProductCard from "./ProductCard";
+
+export default function Products() {
+  return (
+    <div>
+      <ProductCard title="Apple Tablet" desc="Nice Tablet" />
+      <ProductCard title="Amazon Tablet" desc="Better Tablet" />
+      <ProductCard title="Samsung Tablet" desc="Good Tablet" />
+    </div>
+  );
+}
